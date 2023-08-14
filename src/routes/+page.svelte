f2467c3edcb348e85c9003cf0bd78e68839b5f60
Phil <phil@pheedloop.com>
2023-08-14 20:52:25 +0900

    get p5 working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import P5, { type Sketch } from 'p5-svelte';
+	let width = 55;
+	let height = 55;
+
+	const sketch: Sketch = (p5) => {
+		p5.setup = () => {
+			p5.createCanvas(400, 400);
+		};
+
+		p5.draw = () => {
+			p5.ellipse(p5.width / 2, p5.height / 2, width, height);
+		};
+	};
+</script>
+
+<label>
+	Width
+	<input type="range" bind:value={width} min="100" max="1000" step="0.01" />
+	{width}
+</label>
+
+<label>
+	Height
+	<input type="range" bind:value={height} min="100" max="1000" step="0.01" />
+	{height}
+</label>
+
+<h1>Sketch</h1>
+<P5 {sketch} />
