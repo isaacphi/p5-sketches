315d3c16912d011ef76aa83ab9b467d0b670a670
Phil <phil@pheedloop.com>
2023-08-16 13:27:37 +0900

    improvements to gravel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,39 @@
 <script lang="ts">
   import P5, { type Sketch } from 'p5-svelte';
 
-  let canvasWidth = 400;
-  let canvasHeight = 800;
+  let canvasWidth = 500;
+  let canvasHeight = 1000;
 
   let horizontalNum = 12;
   let verticalNum = 22;
 
-  let cubeSide = 10;
+  let cubeSide = 30;
+  let noiseScale = 0.01;
 
   const sketch: Sketch = (p5) => {
     p5.setup = () => {
       p5.createCanvas(canvasWidth, canvasHeight);
-      p5.angleMode(p5.DEGREES);
+      // p5.angleMode(p5.DEGREES);
+      p5.rectMode(p5.CENTER);
 
       let i = 0;
       for (i; i < horizontalNum * verticalNum; i++) {
+        p5.push();
+
         let xPos = (i % horizontalNum) * cubeSide;
         let yPos = Math.floor(i / horizontalNum) * cubeSide;
-        console.log(Math.floor(i / verticalNum) * cubeSide);
-        p5.rotate(i);
-        p5.square(xPos, yPos, cubeSide);
+
+        let rotNoise = p5.noise(xPos * noiseScale, yPos * noiseScale) - 0.5;
+        let xNoise = p5.noise(xPos * noiseScale) - 0.5;
+        let yNoise = p5.noise(yPos * noiseScale) - 0.5;
+        console.log(rotNoise, xNoise, yNoise);
+
+        p5.translate(xPos + cubeSide, yPos + cubeSide);
+        p5.translate(xNoise, yNoise);
+        p5.rotate(rotNoise);
+        p5.square(0, 0, cubeSide);
+
+        p5.pop();
       }
     };
     // p5.draw = () => {};
